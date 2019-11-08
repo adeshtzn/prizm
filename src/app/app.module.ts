7c3bbbe712a8643cc5b9402d54931a2d125a1690
Vidit Paranjpe <vidit.paranjpe@tantranzm.com>
2019-11-08 13:32:21 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SiteComponent } from './site/site.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SiteComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
